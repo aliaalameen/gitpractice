4d6475f7fbe76ce3bf051144c8e7c7dd3a71d475
Alya Alameen <s1897022@bioinfmsc5.ed.ac.uk>
2024-10-17 21:48:38 +0100

    exercises from lecture 3

---
+++ linuxexercises.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#how many people are there in the data set:
+awk 'BEGIN{FS="\t";}
+
+{
+if (NF == 7 && $1 != " " && $1 != "name")
+
+{print $0;}
+
+}' example_people_data.tsv | wc -l
+
+#How many people are aged around 30:
+
+awk 'BEGIN{FS="\t";}
+
+{
+if (NF == 7 && $1 != " ")
+{
+if ($6 <= 1994)
+{print $0;}
+}
+}' example_people_data.tsv | wc -l
+
+#How many people are called Jan:
+
+awk 'BEGIN{FS="\t";}
+
+{
+if (NF == 7 && $1 != " ")
+{
+if ($1 == "Jan" && length($1) == 3)
+{print $0;}
+}
+}' example_people_data.tsv | wc -l
+
+
